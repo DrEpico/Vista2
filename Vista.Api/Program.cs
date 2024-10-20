f1dc6e4b970de97d7f8c6dcf1c68c42eee658515
Ali <adadashipour@gmail.com>
2024-10-20 17:53:32 +0100

    Add TrainersController

--- Vista.Api/Program.cs
+++ Vista.Api/Program.cs
@@ -1,3 +1,5 @@
+using Vista.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register database context with the framework
+builder.Services.AddDbContext<TrainersDbContext>();
 
 var app = builder.Build();
 
