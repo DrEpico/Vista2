479d816cd9065ccc0d7de5064195cc6d515778dc
Ali <adadashipour@gmail.com>
2024-10-20 19:06:19 +0100

    Add new DTO's

---
+++ Vista.Api/Dtos/SessionBookingRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vista.Api.Dtos
+{
+    public class SessionBookingRequestDto
+    {
+        [Required]
+        public int SessionId { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime SessionDate { get; set; }
+    }
+}
